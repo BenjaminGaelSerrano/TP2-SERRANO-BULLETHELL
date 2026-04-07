932ff98389c8ac7f114960c515c7e3fe0390a66d
Benjamín-Serrano <167205322+BenjaminGaelSerrano@users.noreply.github.com>
2026-04-06 22:16:35 -0300

    Finalización del fondo Infinito
    Luego de probar muchas formas, investigando descubrí el elemento Parallax2d, que me facilitó todo.

--- fondo_2_desplazante.gd
+++
@@ -1,9 +0,0 @@
-extends TextureRect
-var VelocidadMovimiento = -90
-func _ready() -> void:
-	flip_v = true
-	position.y = 1536
-func _process(delta: float) -> void:
-	position.y += VelocidadMovimiento * delta
-	if position.y <= -1536:
-		position.y += 1536 * 2
